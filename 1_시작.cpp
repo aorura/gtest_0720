38bc1d5df507030ef701916f6f13a05d0cbb7a69
Chansik Yun <chansigi@gmail.com>
2020-07-20 04:08:04 +0000

    .

---
+++ 1_시작.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+
+// 테스트 구성 - xUnit
+//    TestSuite
+//       TestCase 1
+//       TestCase 2
+//
+// Google Test(1.10 버전 이전)
+//    TestCase
+//       Test 1
+//       Test 2
+
+
+// 1. 구글 테스트에서 테스트를 작성하는 방법.
+TEST(TestSuiteName, TestCase1) {
+
+}
+
+TEST(TestSuiteName, TestCase2) {
+
+}
