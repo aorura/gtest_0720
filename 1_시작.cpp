a749b915e6576d8ebc8ce413b9f86c37fcb590ff
Chansik Yun <chansigi@gmail.com>
2020-07-20 04:15:44 +0000

    .

--- 1_시작.cpp
+++ 1_시작.cpp
@@ -14,9 +14,17 @@
 
 // 1. 구글 테스트에서 테스트를 작성하는 방법.
 TEST(TestSuiteName, TestCase1) {
-
+	FAIL();
 }
 
 TEST(TestSuiteName, TestCase2) {
+
+}
+
+TEST(SampleTest, TestCase1) {
+	FAIL() << "작성 중입니다.";
+}
+
+TEST(SampleTest, TestCase2) {
 
 }
