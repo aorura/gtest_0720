25c4b616402853b2b27ffa433d668762111f4397
Chansik Yun <chansigi@gmail.com>
2020-07-22 00:46:54 +0000

    .

--- 16_Mocking.cpp
+++ 16_Mocking.cpp
@@ -8,6 +8,11 @@ struct MP3 {
 	virtual void Stop(int n) = 0;
 
 	virtual std::string GetName() const = 0;
+
+	virtual int add(int a, int b) noexcept = 0;
+
+	virtual std::pair<bool, int> GetPair() const = 0;
+	virtual bool CheckMap(std::map<int, double> a, bool b) = 0;
 };
 
 // MP3* p = ...
@@ -17,14 +22,27 @@ struct MP3 {
 // 1.10 이후
 // - 더 이상 스크립트는 제공되지 않습니다.
 // - MOCK_METHOD(반환타입, 메소드이름, 인자정보, 한정자);
+//   한정자
+//   1. override - 선택
+//   2. const    - 필수
 class MockMP3 : public MP3 {
 public:
+	// 템플릿 인자를 가지고 있는 경우
+	// std::pair<bool, int> GetPair() const override {}
+	MOCK_METHOD((std::pair<bool, int>), GetPair, (), (const, override));
+
+	MOCK_METHOD(bool, CheckMap, ((std::map<int, double> a), bool b), (override));
+
+	// 일반적인 방법.
 	// void Play() override {}
 	MOCK_METHOD(void, Play, (), (override));
 	MOCK_METHOD(void, Stop, (int n), (override));
 
 	// std::string GetName() const override {}
 	MOCK_METHOD(std::string, GetName, (), (const, override));
+
+	// int add(int a, int b) noexcept override {}
+	MOCK_METHOD(int, add, (int a, int b), (noexcept, override));
 };
 
 
