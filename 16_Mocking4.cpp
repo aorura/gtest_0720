71f4f198cdf9e10fc217ed84b066cff03edc1b76
Chansik Yun <chansigi@gmail.com>
2020-07-22 01:46:31 +0000

    .

--- 16_Mocking4.cpp
+++ 16_Mocking4.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 // C++에서 의존성 주입 모델을 구현하는 방법
 //  1. 명시적 인터페이스 - struct(class)
+//   => 실행 시간에 정책을 교체하는 것이 가능하다.
+//   => 가상 함수를 통해 처리되기 때문에, 상대적으로 느리다.
 struct Packet {};
 struct IPacketStream {
 	virtual ~IPacketStream() {}
@@ -28,12 +30,34 @@ public:
 	}
 };
 
+//-----------------------
+#include <gmock/gmock.h>
+
+class MockPacketStream : public IPacketStream {
+public:
+	MOCK_METHOD(void, AppendPacket, (Packet* new_packet), (override));
+	MOCK_METHOD(const Packet*, GetPacket, (size_t packet_number), (const, override));
+};
+
+TEST(PacketReaderTest, MockTest) {
+	PacketReader reader;
+	MockPacketStream mock;
+
+	EXPECT_CALL(mock, AppendPacket(nullptr));
+	EXPECT_CALL(mock, GetPacket(42));
+
+	reader.ReadPacket(&mock, 42);
+}
+
+
+#if 0
 int main() {
 	PacketReader reader;
 	ConcreatePacketStream stream;
 
 	reader.ReadPacket(&stream, 42);
 }
+#endif
 
 
 
--- 16_Mocking5.cpp
+++ 16_Mocking5.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 //  2. 암묵적 인터페이스 - template
+//   => 단위 전략(Policy based Design)
+//   => 컴파일 타임에 정책의 교체가 발생하기 때문에,
+//      런타임에 정책의 교체를 위해서는 별도의 타입을 생성해야한다.
+//   => 가상 함수가 아니기 때문에, 인라인화가 가능하다.
 struct Packet {};
 
 class ConcreatePacketStream {
@@ -23,12 +27,32 @@ public:
 	}
 };
 
+#include <gmock/gmock.h>
+
+class MockPacketStream {
+public:
+	MOCK_METHOD(void, AppendPacket, (Packet* new_packet));
+	MOCK_METHOD(const Packet*, GetPacket, (size_t packet_number), (const));
+};
+
+TEST(PacketReaderTest, MockTest) {
+	PacketReader<MockPacketStream> reader;
+	MockPacketStream mock;
+
+	EXPECT_CALL(mock, AppendPacket(nullptr));
+	EXPECT_CALL(mock, GetPacket(42));
+
+	reader.ReadPacket(&mock, 42);
+}
+
+#if 0
 int main() {
 	PacketReader<ConcreatePacketStream> reader;
 	ConcreatePacketStream stream;
 
 	reader.ReadPacket(&stream, 42);
 }
+#endif
 
 
 
